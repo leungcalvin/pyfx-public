698f1e4803f2e3c7da3a26aac7c15e435636142a
Calvin Leung <calvinl@mit.edu>
2023-02-21 19:35:03 -0800

    CL: add packaging files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyfx",
+    version="0.1.0",
+    author="Calvin Leung",
+    author_email="calvinleung@mit.edu",
+    packages=find_packages(),
+    url="http://github.com/leungcalvin/pyfx",
+    license="LICENSE.txt",
+    description="A Python and HDF5-based VLBI correlator for widefield, transient VLBI",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "numpy >= 1.19.2",
+        "scipy >= 1.5.2",
+        "matplotlib",
+        "h5py",
+        "argparse",
+        "astropy",
+        "baseband-analysis",
+        "difxcalc-wrapper",
+    ],
+    python_requires=">=3.6",
+)
