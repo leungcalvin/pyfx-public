fa2c54f63310536ba33c2782de9f417738e93ba5
Calvin Leung <calvinl@mit.edu>
2023-09-08 20:52:55 +0000

    forgot to ship this

---
+++ tests/test_vlbivis.py
@@ -0,0 +1,11 @@
+def test_import():
+    from coda.core import VLBIVis
+
+def test_compat():
+    import numpy as np
+    from coda.core import VLBIVis
+    a = VLBIVis()
+    dtype = a._dataset_dtypes['time']
+    int_time = np.empty((1024,10),dtype=dtype)
+    assert ('dur_ratio','<f8') in dtype
+    assert ('on_window',bool) in dtype
